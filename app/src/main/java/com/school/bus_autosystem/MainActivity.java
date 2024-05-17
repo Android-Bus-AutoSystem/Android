c937d3a17af8ad2f17a4adf176445b74853db4c0
JaeYoonJung08 <whitecy01@naver.com>
2024-05-17 17:43:41 +0900

    feat : mainpage xml 디자인 추가

--- app/src/main/java/com/school/bus_autosystem/MainActivity.java
+++ app/src/main/java/com/school/bus_autosystem/MainActivity.java
@@ -1,15 +1,29 @@
 package com.school.bus_autosystem;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout buttonLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dev 브랜치 추가
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        buttonLayout = findViewById(R.id.button_layout);
+        buttonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
