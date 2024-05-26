029bc6cba2eb5ba1c9c278086e28b54ea40c34f3
JaeYoonJung08 <whitecy01@naver.com>
2024-05-26 10:17:58 +0900

    feat : 자기 자신 위치 코드 추가_실행 안됨

--- app/src/main/java/com/school/bus_autosystem/MapActivity.java
+++ app/src/main/java/com/school/bus_autosystem/MapActivity.java
@@ -1,16 +1,21 @@
 package com.school.bus_autosystem;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.kakao.vectormap.Compass;
 import com.kakao.vectormap.KakaoMap;
 import com.kakao.vectormap.KakaoMapReadyCallback;
@@ -20,21 +25,21 @@ import com.kakao.vectormap.MapLifeCycleCallback;
 import com.kakao.vectormap.MapView;
 import com.kakao.vectormap.ScaleBar;
 import com.kakao.vectormap.camera.CameraPosition;
+import com.kakao.vectormap.camera.CameraUpdate;
 import com.kakao.vectormap.camera.CameraUpdateFactory;
-import com.kakao.vectormap.label.LabelLayer;
-import com.kakao.vectormap.label.LabelOptions;
-import com.kakao.vectormap.label.LabelStyle;
-import com.kakao.vectormap.label.TrackingManager;
 
-public class MapActivity  extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
     MapView mapView;
     KakaoMap kakaoMap;
     TextView location_busstop2, near_busstop, location_busstop;
     private ScaleBar scaleBar;
     private Compass compass;
     private int startZoomLevel = 15;
-    private LatLng startPosition = LatLng.from(37.394660,127.111182);   // 판교역
+    private LatLng startPosition = LatLng.from(37.394660, 127.111182);   // 판교역
 
+//    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+//    private FusedLocationProviderClient fusedLocationClient;
+//    private LatLng currentLocation;
 
     // MapReadyCallback 을 통해 지도가 정상적으로 시작된 후에 수신할 수 있다.
     private KakaoMapReadyCallback readyCallback = new KakaoMapReadyCallback() {
@@ -43,7 +48,7 @@ public class MapActivity  extends AppCompatActivity {
             kakaoMap = map;
 
             scaleBar = kakaoMap.getScaleBar();
-            scaleBar.setPosition(MapGravity.LEFT|MapGravity.BOTTOM, 20, 20);
+            scaleBar.setPosition(MapGravity.LEFT | MapGravity.BOTTOM, 20, 20);
             scaleBar.show();
             compass = kakaoMap.getCompass();
             compass.show();
@@ -58,6 +63,9 @@ public class MapActivity  extends AppCompatActivity {
                     + kakaoMap.getCameraPosition().getPosition().toString());
             Log.i("k3f", "startZoomLevel: "
                     + kakaoMap.getZoomLevel());
+//            if (currentLocation != null) {
+//                updateMapLocation();
+//            }
         }
 
         @NonNull
@@ -105,11 +113,77 @@ public class MapActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         setTitle("Map Start");
 
-         near_busstop = (TextView) findViewById(R.id.near_busstop_text);
-         location_busstop = (TextView) findViewById(R.id.location_busstop_text);
-         location_busstop2 = (TextView) findViewById(R.id.location_busstop_text2);
+//         near_busstop = (TextView) findViewById(R.id.near_busstop_text);
+//         location_busstop = (TextView) findViewById(R.id.location_busstop_text);
+//         location_busstop2 = (TextView) findViewById(R.id.location_busstop_text2);
 
         mapView = findViewById(R.id.map_view);
+
+//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION}, LOCATION_PERMISSION_REQUEST_CODE);
+//        } else {
+//            getLastKnownLocation();
+//        }
+
         mapView.start(lifeCycleCallback, readyCallback);
+
     }
+
+//    private void getLastKnownLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        fusedLocationClient.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        if (location != null) {
+//                            currentLocation = LatLng.from(location.getLatitude(), location.getLongitude());
+//                            if (kakaoMap != null) {
+//                                updateMapLocation();
+//                            }
+//                        }
+//                    }
+//                });
+//    }
+
+//    private void updateMapLocation() {
+//        CameraPosition cameraPosition = new CameraPosition.Builder()
+//                .target(currentLocation)
+//                .zoom(15)
+//                .build();
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+//        kakaoMap.moveCamera(cameraUpdate);
+//
+//        near_busstop.setText(String.valueOf(currentLocation.getLatitude()));
+//        location_busstop.setText(String.valueOf(currentLocation.getLongitude()));
+//        location_busstop2.setText(String.valueOf(kakaoMap.getZoomLevel()));
+//
+////        kakaoMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+////        near_busstop.setText(String.valueOf(currentLocation.getLatitude()));
+////        location_busstop.setText(String.valueOf(currentLocation.getLongitude()));
+////        location_busstop2.setText(String.valueOf(kakaoMap.getZoomLevel()));
+//    }
+
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                getLastKnownLocation();
+//            } else {
+//                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
